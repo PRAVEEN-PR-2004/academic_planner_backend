764d5b7d3d263f7da28a918a580a0d364137b600
PRAVEEN-PR-2004 <praveen2004ttp@gmail.com>
2025-04-26 21:33:42 +0530

    .env  change

--- config/db.js
+++ config/db.js
@@ -1,18 +1,15 @@
 const mongoose = require("mongoose");
+require("dotenv").config(); // Load environment variables
 
-// MongoDB URI
-const dbURI =
-  "mongodb+srv://praveen2004ttp:uRQdWpkmsuK2rDOa@cluster0.uuc5gqh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"; // Replace with your MongoDB URI
+const dbURI = process.env.MONGO_URI; // Get MongoDB URI from .env
 
-// Function to connect to MongoDB
 const connectDB = async () => {
   try {
-    // Connect to MongoDB
     await mongoose.connect(dbURI, {});
     console.log("MongoDB connected successfully!");
   } catch (err) {
     console.error("Error connecting to MongoDB:", err.message);
-    process.exit(1); // Exit the process if the connection fails
+    process.exit(1);
   }
 };
 
